ed92efaf77ad89aa1c0b8fdd491e5087b60f50e0
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-27 11:23:30 -0500

    Types: Funcion clasica en JavaScript

---
+++ types/js/scripts.js
@@ -0,0 +1,11 @@
+/* Types:
+ * Función Clasica de JavaScript no usa tipos
+ */
+/* Función JS*/
+function add(x, y) {
+    return x + y;
+}
+/* Ejecución */
+console.log(add(1, 4)); // 5
+console.log(add('Birth', 'day')); // 'Birthday'
+console.log(add(1, 'day')); // '1day'
