7833e1d52b89eabba20dfc99b1a07c80dc02ea5c
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-27 13:33:18 -0500

    Types: Problema de mapear arrays dinámicos en JavaScript
    DECLARACION DE TIPOS
    
    let algunString          : string;
    let algunNumero          : number;
    let algunTrueOrFalse     : boolean;
    let algunArrayDinamico   : Array<any>;
    let algunArrayStrings    : Array<string>;
    let algunArrayNumeros    : Array<number>;
    let algunArrayObjetos    : Array<object>;
    let algunElemento        : Element;
    let algunObjeto          : object;
    let algunaCosaNula       : null;
    let algunaCosaIndefinida : undefined;
    let algunaCosaVacia      : void;
    
    let algunaCosaDinamica   : any;

--- types/js/scripts.js
+++ types/js/scripts.js
@@ -1,50 +1,25 @@
 /* Types:
- * Como declarar variales y constantes en TypeScript
+ * Ejemplo: Problema que supone mapear y extraer valores de un
+ *          Array dinámico en JavaScript
  */
-/* Los tipos en JavaScript son dinámicos
- * por eso podemos asignar diferentes valores a una sola variable */
-var variable_js;
-console.group('En JavaScript ');
-variable_js = 'Hola desde JavaScript';
-console.log(variable_js); // JS lo interpreta como una cadena (string)
-variable_js = 59430;
-console.log(variable_js); // JS lo interpreta como un número entero
-variable_js = ['Manzana', 13, 0.192, false];
-console.log(variable_js); // JS lo interpreta como Array de valores de diferente tipo
-variable_js = 23.1;
-console.log(variable_js); // JS lo interpreta como un número decimal
-variable_js = { nombre: 'Juan', edad: 40, profesion: 'Desarrollador' };
-console.log(variable_js); // JS lo interpreta como un Objeto
-variable_js = true;
-console.log(variable_js); // JS lo interpreta como un valore booleano
-console.groupEnd();
-/* Los tipos en TypeScript son estáticos
- * por eso podemos NO asignar diferentes valores a una sola variable */
-var saludo_ts;
-var numero_ts;
-var tupla_ts;
-var numeros_ts;
-var frutas_ts;
-var verduras_ts;
-var objeto_ts;
-var boleano;
-console.group('En TypeScript ');
-saludo_ts = 'Hola desde TypeScript';
-console.log(saludo_ts);
-entero_ts = 59430;
-console.log(entero_ts);
-tupla_ts = ['manzanas', 32];
-console.log(tupla_ts);
-numeros_ts = [1, 0.9, 3.2, 10];
-console.log(numeros_ts);
-frutas_ts = ['Manzana', 'Mora', 'Mango'];
-console.log(frutas_ts);
-verduras_ts = ['Acelgas', 'Espinacas', 'Apio'];
-console.log(verduras_ts);
-numero_ts = 23.1;
-console.log(numero_ts);
-objeto_ts = { nombre: 'Juan', edad: 40, profesion: 'Desarrollador' };
-console.log(objeto_ts);
-boleano_ts = true;
-console.log(boleano_ts);
-console.groupEnd();
+/* Array de tipos dinámicos */
+var numArray = [
+    1,
+    2,
+    'interrupting cow',
+    {
+        index: 3,
+        title: 'interrupting cow'
+    }
+];
+/* Función para agregar un 1 a la cuenta */
+var add1 = function (num) { return 1 + num; };
+/* Mapeo de datos */
+numArray.map(function (item) {
+    console.log('numArray[ ' + item + ' ]', add1(item));
+    return item;
+});
+console.log('Array Mapeado ', numArray.map(function (item) {
+    add1(item);
+    return item;
+}));
