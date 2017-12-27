d49246e8ef31da9da38b5ca7d4423c200a295911
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-27 15:47:46 -0500

    Types: Tipos Any (Tipos dinámicos en TS)

--- types/js/scripts.js
+++ types/js/scripts.js
@@ -1,56 +1,21 @@
-/* Types: Tipos Básicos */
-var unBoleano = false; // Boolean Type
-var unEntero = 9; // Number Type (entero)
-var unDecimal = 2.9; // Number Type (decimal)
-var unHexadecimal = 0xf00d; // Number Type (hexadecimal)
-var unOctal = 484; // Number Type (octal)
-var unBinario = 10; // Number Type (binario)
-var unaCadena = 'Me gusta el color azul'; // String Type
-var primerArregloNumeros = [1, 2, 3]; // Array Number Type
-var segundoArregloNumeros = [4.6, 5.5, 6.4]; // Array Number Type
-/* Funciona igual para los otros tipos incluido del dinámico 'any' */
-var unaTupla = ['nombre', 'Juan Carlos']; // Tuple [ string type, string type ]
-var Color;
-(function (Color) {
-})(Color || (Color = {}));
-{
-    Rojo = 1,
-        Verde = 2,
-        Azul = 4;
-}
-var color1 = Color.Verde; //
-var color2 = Color[2]; // Azul
-var unNulo = null;
-var unIndefinido = undefined;
-var primerVacio = null;
-var segundoVacio = undefined;
-console.group('Boolean');
-console.log('unBoleano ', unBoleano);
-console.groupEnd();
-console.group('String');
-console.log('unEntero ', unEntero);
-console.log('unDecimal ', unDecimal);
-console.log('unHexadecimal ', unHexadecimal);
-console.log('unOctal ', unOctal);
-console.log('unBinario ', unBinario);
-console.groupEnd();
-console.group('String');
-console.log('unaCadena ', unaCadena);
-console.groupEnd();
-console.group('Array<number>');
-console.log('primerArregloNumeros ', primerArregloNumeros);
-console.log('segundoArregloNumeros ', segundoArregloNumeros);
-console.groupEnd();
-console.group('tuple [ string string ]');
-console.log('unaTupla ', unaTupla);
-console.groupEnd();
-console.group('enum');
-console.log('color1 ', color1);
-console.log('color2 ', color2);
-console.groupEnd();
-console.group('null, undefined, void');
-console.log('unNulo ', unNulo);
-console.log('unIndefinido ', unIndefinido);
-console.log('primerVacio ', primerVacio);
-console.log('segundoVacio ', segundoVacio);
-console.groupEnd();
+/* Types: Cualquier tipo */
+/* Types: Cualquier tipo */
+var variable;
+variable = 10;
+console.log('variable ', variable);
+variable = 'Esta es una variable tipo "any" de TypeScript';
+console.log('variable ', variable);
+variable = ['hi', 'hello'];
+console.log('variable ', variable);
+variable = true;
+console.log('variable ', variable);
+variable = { nombre: 'Alicia', edad: 19, casada: true };
+console.log('variable ', variable);
+variable = null;
+console.log('variable ', variable);
+//variable = void;                        // Genera un Error            
+//console .log( 'variable ', variable );
+variable = 14.9;
+console.log('variable ', variable);
+variable = undefined;
+console.log('variable ', variable);
--- types/js/scripts.ts
+++ types/js/scripts.ts
@@ -1,68 +1,24 @@
-/* Types: Tipos Básicos */
+/* Types: Cualquier tipo */
 
-let unBoleano : boolean = false;    // Boolean Type
+/* Types: Cualquier tipo */
 
-let unEntero : number = 9;          // Number Type (entero)
-let unDecimal : number = 2.9;       // Number Type (decimal)
+let variable: any;
 
-let unHexadecimal : number = 0xf00d;  // Number Type (hexadecimal)
-let unOctal : number = 0o744;         // Number Type (octal)
-let unBinario : number = 0b1010;      // Number Type (binario)
-
-let unaCadena : string = 'Me gusta el color azul';   // String Type
-
-let primerArregloNumeros : number[] = [ 1, 2 ,3 ];              // Array Number Type
-let segundoArregloNumeros : Array<number> = [ 4.6, 5.5, 6.4 ];  // Array Number Type
-/* Funciona igual para los otros tipos incluido del dinámico 'any' */
-
-let unaTupla : [ string, string ] = [ 'nombre', 'Juan Carlos' ];  // Tuple [ string type, string type ]
-
-enum Color = {    // Enum Type
-  Rojo = 1,
-  Verde = 2,
-  Azul = 4
-}
-let color1: Color = Color.Verde;    //
-let color2: Color = Color[ 2 ];     // Azul
-
-let unNulo : null = null;
-let unIndefinido : undefined = undefined;
-let primerVacio : null = null;
-let segundoVacio : undefined = undefined;
-
-console .group( 'Boolean' );
-  console .log( 'unBoleano ', unBoleano );
-console .groupEnd();
-
-console .group( 'String' );
-  console .log( 'unEntero ', unEntero );
-  console .log( 'unDecimal ', unDecimal );
-  console .log( 'unHexadecimal ', unHexadecimal );
-  console .log( 'unOctal ', unOctal );
-  console .log( 'unBinario ', unBinario );
-console .groupEnd();
-
-console .group( 'String' );
-  console .log( 'unaCadena ', unaCadena );
-console .groupEnd();
-
-console .group( 'Array<number>' );
-  console .log( 'primerArregloNumeros ', primerArregloNumeros );
-  console .log( 'segundoArregloNumeros ', segundoArregloNumeros );
-console .groupEnd();
-
-console .group( 'tuple [ string string ]' );
-  console .log( 'unaTupla ', unaTupla );
-console .groupEnd();
-
-console .group( 'enum' );
-  console .log( 'color1 ', color1 );
-  console .log( 'color2 ', color2 );
-console .groupEnd();
-
-console .group( 'null, undefined, void' );
-  console .log( 'unNulo ', unNulo );
-  console .log( 'unIndefinido ', unIndefinido );
-  console .log( 'primerVacio ', primerVacio );
-  console .log( 'segundoVacio ', segundoVacio );
-console .groupEnd();
+variable = 10;
+console .log( 'variable ', variable );
+variable = 'Esta es una variable tipo "any" de TypeScript';
+console .log( 'variable ', variable );
+variable = [ 'hi', 'hello' ];
+console .log( 'variable ', variable );
+variable = true;
+console .log( 'variable ', variable );
+variable = { nombre: 'Alicia', edad: 19, casada: true };
+console .log( 'variable ', variable );
+variable = null;
+console .log( 'variable ', variable );
+//variable = void;                        // Genera un Error            
+//console .log( 'variable ', variable );
+variable = 14.9;
+console .log( 'variable ', variable );
+variable = undefined;
+console .log( 'variable ', variable );
