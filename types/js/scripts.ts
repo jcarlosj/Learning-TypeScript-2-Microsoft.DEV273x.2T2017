968586c19a4028f933eac1128da7a453a039c660
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-27 16:00:03 -0500

    Types: Assertions (Aserciones de tipo)

--- types/js/scripts.ts
+++ types/js/scripts.ts
@@ -1,24 +1,16 @@
 /* Types: Cualquier tipo */
 
-/* Types: Cualquier tipo */
+/* Types: Assertions
+ * Una aserción de tipo es como un tipo de conversión. No tiene impacto
+   en tiempo de ejecución y lo usa exclusivamente TypeScript Se usan los
+    angulares (angle-bracket) para indicar la aserción de tipo
+ * "Creeme que sé lo que estoy haciendo o yo sé que es eso" */
 
-let variable: any;
+let algunValor : any = "Esta es una cadena de texto";
+let cantidadCaracteres_1 : number = ( <string> algunValor ) .length;    // Forma 1
+let cantidadCaracteres_2 : number = ( algunValor as string ) .length;   // Forma 2
 
-variable = 10;
-console .log( 'variable ', variable );
-variable = 'Esta es una variable tipo "any" de TypeScript';
-console .log( 'variable ', variable );
-variable = [ 'hi', 'hello' ];
-console .log( 'variable ', variable );
-variable = true;
-console .log( 'variable ', variable );
-variable = { nombre: 'Alicia', edad: 19, casada: true };
-console .log( 'variable ', variable );
-variable = null;
-console .log( 'variable ', variable );
-//variable = void;                        // Genera un Error            
-//console .log( 'variable ', variable );
-variable = 14.9;
-console .log( 'variable ', variable );
-variable = undefined;
-console .log( 'variable ', variable );
+console .group( 'Type Assertions' );
+  console .log( 'cantidadCaracteres_1 ', cantidadCaracteres_1 );
+  console .log( 'cantidadCaracteres_2 ', cantidadCaracteres_2 );
+console .groupEnd();
