0f104508aad07ca0c3f531bdcca3d4af8e361422
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-28 15:48:17 -0500

    Types: Tipos complejos - Excess Property Checks: SOLUCION por paso de parámetros que no existen en la 'Interface' usando 'Asserts'

--- types/js/scripts.js
+++ types/js/scripts.js
@@ -15,5 +15,5 @@ function crearCuadrado(config) {
     return cuadrado; // Objeto
 }
 // Ejecuta la función y se le pasa a un objeto con las características mínimas que exije la misma
-var primerCuadrado = crearCuadrado({ colour: 'blue', lado: 5 }); // ERROR: la propiedad 'colour' no existe en el tipo 'Cuadrado' (Interface).
-console.log('primerCuadrado ', primerCuadrado);
+var primerCuadrado = crearCuadrado({ colour: 'blue', lado: 5 }); // Usamos una aserción (para evitar el ERROR)
+console.log('primerCuadrado ', primerCuadrado); // Pero la nueva propiedad que se pasa y que no tiene la 'Interface' no se verá reflejada
--- types/js/scripts.ts
+++ types/js/scripts.ts
@@ -25,6 +25,6 @@ function crearCuadrado( config: Cuadrado ) : { color: string, area: number } {
 }
 
 // Ejecuta la función y se le pasa a un objeto con las características mínimas que exije la misma
-let primerCuadrado = crearCuadrado({ colour: 'blue', lado: 5 });    // ERROR: la propiedad 'colour' no existe en el tipo 'Cuadrado' (Interface).
+let primerCuadrado = crearCuadrado({ colour: 'blue', lado: 5 } as Cuadrado );    // Usamos una aserción (para evitar el ERROR)
 
-console .log( 'primerCuadrado ', primerCuadrado );
+console .log( 'primerCuadrado ', primerCuadrado );    // Pero la nueva propiedad que se pasa y que no tiene la 'Interface' no se verá reflejada
