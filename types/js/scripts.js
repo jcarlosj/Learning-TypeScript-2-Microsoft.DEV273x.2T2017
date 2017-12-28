70069fce1421b3634762f48a550a86d58be8660f
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-28 16:33:44 -0500

    Types: Tipos complejos - ERROR de implementación de la 'Interface' en la Clase

--- types/js/scripts.js
+++ types/js/scripts.js
@@ -1,22 +1,6 @@
 /* Types: Class Types */
-/* Define clase que hereda de una 'Interface'
- * Al heredar la 'Interface' obliga a la clase a declarar los atributos y métodos
- * que esta posee en la clase en la que se implementa */
 var Reloj = /** @class */ (function () {
-    /* Constructor*/
     function Reloj(horas, minutos) {
     }
-    /* setTime es un método oblicado a definirse por la 'Interface' */
-    Reloj.prototype.setTime = function (tiempo) {
-        this.tiempoActual = tiempo;
-    };
-    /* getTiempo se declaró pero no esta obligado a ser definido por la'Interface'*/
-    Reloj.prototype.getTime = function () {
-        return this.tiempoActual;
-    };
     return Reloj;
 }());
-/* Instancia */
-var reloj = new Reloj(10, 30);
-/* Resultado */
-console.log('reloj ', reloj);
--- types/js/scripts.ts
+++ types/js/scripts.ts
@@ -1,36 +1,11 @@
 /* Types: Class Types */
 
-/* Define Interface
- * La interface describe el lado público de una clase */
-interface RelojInterface {
+/* Define Interface */
+interface RelojConstructor {
+  new ( horas: number, minutos: number );
+}
+
+class Reloj implements RelojConstructor {
   tiempoActual: Date;
-  setTime( tiempo: Date );
-}
-
-/* Define clase que hereda de una 'Interface'
- * Al heredar la 'Interface' obliga a la clase a declarar los atributos y métodos
- * que esta posee en la clase en la que se implementa */
-class Reloj implements RelojInterface {
-  /* Atributos */
-  tiempoActual: Date;       // Es obligatorio definirla por la 'Interface'
-
-  /* Constructor*/
   constructor( horas: number, minutos: number ) {}
-
-  /* setTime es un método oblicado a definirse por la 'Interface' */
-  setTime( tiempo: Date ) {
-    this .tiempoActual = tiempo;
-  }
-
-  /* getTiempo se declaró pero no esta obligado a ser definido por la'Interface'*/
-  getTime() {
-    return this .tiempoActual;
-  }
-
 }
-
-/* Instancia */
-let reloj = new Reloj( 10, 30 );
-
-/* Resultado */
-console .log( 'reloj ', reloj );
