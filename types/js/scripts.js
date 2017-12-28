21447775fa249b0cf12748828d8bf48f78042e20
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-28 16:37:52 -0500

    Types: Tipos complejos - ERROR Diferencia entre el lado estático y la instancia de clase

--- types/js/scripts.js
+++ types/js/scripts.js
@@ -4,3 +4,8 @@ var Reloj = /** @class */ (function () {
     }
     return Reloj;
 }());
+/* ERROR:
+types/js/scripts.ts(11,7): error TS2420: Class 'Reloj' incorrectly implements interface 'RelojConstructor'.
+Type 'Reloj' provides no match for the signature 'new (horas: number, minutos: number): any'.
+16:35:51 - Compilation complete. Watching for file changes.
+ */
--- types/js/scripts.ts
+++ types/js/scripts.ts
@@ -1,6 +1,9 @@
 /* Types: Class Types */
 
-/* Define Interface */
+/* Define Interface
+ * El ERROR se produce por que cuando la clase implementa una 'Interface'
+ * solo se verifica el lado de la instancia de la clase. Como el constructor
+ * se encuentra en el lado estático, no esta incluido en este control */
 interface RelojConstructor {
   new ( horas: number, minutos: number );
 }
@@ -9,3 +12,9 @@ class Reloj implements RelojConstructor {
   tiempoActual: Date;
   constructor( horas: number, minutos: number ) {}
 }
+
+/* ERROR:
+types/js/scripts.ts(11,7): error TS2420: Class 'Reloj' incorrectly implements interface 'RelojConstructor'.
+Type 'Reloj' provides no match for the signature 'new (horas: number, minutos: number): any'.
+16:35:51 - Compilation complete. Watching for file changes.
+ */
