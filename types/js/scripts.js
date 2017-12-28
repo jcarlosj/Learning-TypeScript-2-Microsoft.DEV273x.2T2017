937c744be6f491c4eaa1d111e514472f21c183d5
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-28 17:19:28 -0500

    Types: Tipos complejos - Function Types (Especificando tipos de entrada y salida)

--- types/js/scripts.js
+++ types/js/scripts.js
@@ -1,31 +1,11 @@
-/* Types: Class Types
- * Trabajamos directamente con el lado estático de la clase, osea el constructor()
- * */
-/* Definición de dos clases (RelojDigital, RelojAnalogo) que hereda de 'RelojConstructor' */
-var RelojDigital = /** @class */ (function () {
-    function RelojDigital(horas, minutos) {
-    }
-    RelojDigital.prototype.tick = function () {
-        return 'beep beep';
-    };
-    return RelojDigital;
-}());
-var RelojAnalogo = /** @class */ (function () {
-    function RelojAnalogo(horas, minutos) {
-    }
-    RelojAnalogo.prototype.tick = function () {
-        return 'tick tock';
-    };
-    return RelojAnalogo;
-}());
-/* Función que crea las instancias del tipo que se le pasan */
-function crearReloj(constructorReloj, horas, minutos) {
-    return new constructorReloj(horas, minutos); // Equivale a: new RelojDigital( horas, minutos ) o new RelojAnalogo( horas, minutos )
-}
-/* Ejecutamos cada una de las clases sin hacer instanciación de las mismas */
-var digital = crearReloj(RelojDigital, 12, 17);
-var analogo = crearReloj(RelojAnalogo, 7, 32);
-console.group('Reloj');
-console.log('Digital ', digital.tick());
-console.log('Analogo ', analogo.tick());
-console.groupEnd();
+/* Types: Function Types
+ * Las interfaces pemiten describir los tipos de funciones de un Objeto */
+/* Crea una variable del tipo de la 'Interface' */
+var busqueda;
+/* Le asignamos una funcion con los parametros definidos */
+busqueda = function (fuente, subcadena) {
+    var resultado = fuente.search(subcadena);
+    return resultado > -1;
+};
+/* Resultado */
+console.log('busqueda ', busqueda);
--- types/js/scripts.ts
+++ types/js/scripts.ts
@@ -1,39 +1,19 @@
-/* Types: Class Types
- * Trabajamos directamente con el lado estático de la clase, osea el constructor()
- * */
+/* Types: Function Types
+ * Las interfaces pemiten describir los tipos de funciones de un Objeto */
 
-/* Definición de Interface que controla la creación del constructor */
-interface RelojConstructor {
-  new ( horas: number, minutos: number ) : RelojInterface;    /* Heredando de la Interface (RelojInterface) */
-}
-interface RelojInterface {
-  tick();
+/* Crea 'Interface' */
+interface Buscar {
+ ( fuente: string, subcadena: string ) : boolean;   // Firma de llamada: lista de parámetros que requiere (nombres y tipos)
 }
 
-/* Definición de dos clases (RelojDigital, RelojAnalogo) que hereda de 'RelojConstructor' */
-class RelojDigital implements RelojConstructor {
-  constructor( horas: number, minutos: number ) {}
-  tick() {
-    return 'beep beep';
-  }
-}
-class RelojAnalogo implements RelojConstructor {
-  constructor( horas: number, minutos: number ) {}
-  tick() {
-    return 'tick tock';
-  }
+/* Crea una variable del tipo de la 'Interface' */
+let busqueda: Buscar;
+
+/* Le asignamos una funcion con los parametros definidos */
+busqueda = function( fuente: string, subcadena: string ) : any {
+  let resultado = fuente .search( subcadena );
+  return resultado > -1;
 }
 
-/* Función que crea las instancias del tipo que se le pasan */
-function crearReloj( constructorReloj: RelojConstructor, horas: number, minutos: number ) : RelojInterface {
-  return new constructorReloj( horas, minutos );    // Equivale a: new RelojDigital( horas, minutos ) o new RelojAnalogo( horas, minutos )
-}
-
-/* Ejecutamos cada una de las clases sin hacer instanciación de las mismas */
-let digital = crearReloj( RelojDigital, 12, 17 );
-let analogo = crearReloj( RelojAnalogo, 7, 32 );
-
-console .group( 'Reloj' );
-  console .log( 'Digital ', digital .tick() );
-  console .log( 'Analogo ', analogo .tick() );
-console .groupEnd();
+/* Resultado */
+console .log( 'busqueda ', busqueda );
