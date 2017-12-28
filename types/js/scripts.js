036da90263bf2d9a574366721fc6885008ca69b7
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-27 19:43:20 -0500

    Types: Readonly properties (Propiedades de solo lectura)

--- types/js/scripts.js
+++ types/js/scripts.js
@@ -1,10 +1,25 @@
-/* Types: Interfaces */
-// Creamos un objeto literal
-var punto1 = {
-    coordenada_X: 10,
-    coordenada_Y: 20
-};
-/* Mostramos el Objeto */
-console.log('punto1 ', punto1);
-/* Asignamos nuevo valor a X */
-//  punto1 .coordenada_X = 5;      // ERROR: por que la propiedad es solo de lectura
+/* Types: Readonly properties */
+var a = [1, 2, 3, 4];
+var b = [7, 8, 9, 0];
+var ro = a;
+console.log('let a: number[] = [1, 2, 3, 4]; ... ', a);
+console.log('let b: number[] = [7, 8, 9, 0]; ... ', b);
+console.group('ReadonlyArray<number>');
+console.log('------------------------------------');
+console.log('*let ro: ReadonlyArray<number> = a; ... ', ro);
+console.groupEnd();
+/* Los siguientes ejemplos generarán errores pues no se les puede Asignar valores
+   a variables designadas como de solo lectura
+
+ro[0] = 12;         // ERROR!
+ro.push(5);         // ERROR!
+ro.length = 100;    // ERROR!
+a = ro;             // ERROR!
+*/
+// EXCEPCIÓN: Usamos una aserción de tipo pára anular la propiedad de solo escritura
+a = ro;
+b = ro;
+console.group('Anulacion "readonly" usando un "Type Assertion"');
+console.log('a = ro as number[]; ... ', a);
+console.log('b = <number[]> ro; ... ', b);
+console.groupEnd();
--- types/js/scripts.ts
+++ types/js/scripts.ts
@@ -1,20 +1,32 @@
-/* Types: Interfaces */
+/* Types: Readonly properties */
 
-// Interface
-interface Punto {
-  /* Define las propiedades como de solo lectura */
-  readonly coordenada_X : number;
-  readonly coordenada_Y : number;
-}
+let a: number[] = [1, 2, 3, 4];
+let b: number[] = [7, 8, 9, 0];
 
-// Creamos un objeto literal
-let punto1 : Punto = {
-  coordenada_X: 10,
-  coordenada_Y: 20
-};
+let ro: ReadonlyArray<number> = a;
 
-/* Mostramos el Objeto */
-console .log( 'punto1 ', punto1 );
+console .log( 'let a: number[] = [1, 2, 3, 4]; ... ', a );
+console .log( 'let b: number[] = [7, 8, 9, 0]; ... ', b );
 
-/* Asignamos nuevo valor a X */
-//  punto1 .coordenada_X = 5;      // ERROR: por que la propiedad es solo de lectura
+console .group( 'ReadonlyArray<number>' );
+  console .log( '------------------------------------' );
+  console .log( '*let ro: ReadonlyArray<number> = a; ... ', ro );
+console .groupEnd();
+
+/* Los siguientes ejemplos generarán errores pues no se les puede Asignar valores
+   a variables designadas como de solo lectura
+
+ro[0] = 12;         // ERROR!
+ro.push(5);         // ERROR!
+ro.length = 100;    // ERROR!
+a = ro;             // ERROR!
+*/
+
+// EXCEPCIÓN: Usamos una aserción de tipo pára anular la propiedad de solo escritura
+a = ro as number[];
+b = ( <number[]> ro );
+
+console .group( 'Anulacion "readonly" usando un "Type Assertion"' );
+  console .log( 'a = ro as number[]; ... ', a );
+  console .log( 'b = <number[]> ro; ... ', b );
+console .groupEnd();
