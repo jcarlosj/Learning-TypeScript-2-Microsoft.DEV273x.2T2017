df5635a634619002767c85b63eaad8946a784ccc
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-29 11:53:15 -0500

    Types: Tecnicas Avanzadas - Clase creada apartir de variable declarada con el mismo tipo de la Instancia

--- types/js/scripts.js
+++ types/js/scripts.js
@@ -1,55 +1,20 @@
 /* Types: Classes
- * Las clases abstractas son clases base de las cuales pueden derivarse otras clases
- * No pueden ser instanciados directamente. A diferencia de una interfaz, una clase
- * abstracta puede contener detalles de implementación para sus miembros. La palabra
- * clave 'abstract' se usa para definir estas clases, así como también los métodos
- * dentro de la misma */
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = Object.setPrototypeOf ||
-        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-/* Definición de la clase abstracta */
-var Departamento = /** @class */ (function () {
+ * Técnicas Avanzadas */
+/* Contructor functions
+ * Cuando se declara una clase en TS, en realidad está creando múltiples
+ * declaraciones al mismo tiempo. El primero es el tipo de la instancia
+ * de la clase */
+var Saludar = /** @class */ (function () {
     /* Constructor */
-    function Departamento(nombreDelDepartamento) {
-        this.nombre = nombreDelDepartamento;
+    function Saludar(mensajeSaludo) {
+        this.saludo = mensajeSaludo;
     }
     /* Métodos */
-    Departamento.prototype.imprimirNombre = function () {
-        console.log('Departamento de: ', this.nombre);
+    Saludar.prototype.darSaludo = function () {
+        return 'Hola, ' + this.saludo;
     };
-    return Departamento;
+    return Saludar;
 }());
-/* Definición de una clase que hereda de una clase abstracta */
-var DepartamentoContable = /** @class */ (function (_super) {
-    __extends(DepartamentoContable, _super);
-    /* Constructor */
-    function DepartamentoContable() {
-        return _super.call(this, 'Contabilidad y auditoría') || this;
-    }
-    /* Métodos */
-    DepartamentoContable.prototype.imprimirReunion = function () {
-        console.log('Este departamento se reúne todos los lunes a las 10am');
-    };
-    DepartamentoContable.prototype.generarReportes = function () {
-        console.log('Generación de informes contables');
-    };
-    return DepartamentoContable;
-}(Departamento));
-/* Crea referencia a una clase de tipo abstracto */
-var departamento;
-/* ERRORES que no se deben cometer */
-// departamento1 = new Departamento();  // No se puede crear una instancia a partir de una clase abstracta
-// departamento .generarReportes();     // Este método no esta declarado como abstracto por lo que no se considera existente
-//departamento .imprimirNombre();
-// departamento .imprimirReunion();     // Este método no esta declarado como abstracto por lo que no se considera existente
-/* Crea Instancia apartir de una clase que hereda de una clase abstracta */
-var departamento2 = new DepartamentoContable();
-departamento2.imprimirNombre();
-departamento2.imprimirReunion();
+var saludar; // Creamos una variable con el tipo de la Instancia que vamos a crear
+saludar = new Saludar('Mundo!'); // Creamos la Instancia
+console.log(saludar.darSaludo());
--- types/js/scripts.ts
+++ types/js/scripts.ts
@@ -1,51 +1,25 @@
 /* Types: Classes
- * Las clases abstractas son clases base de las cuales pueden derivarse otras clases
- * No pueden ser instanciados directamente. A diferencia de una interfaz, una clase
- * abstracta puede contener detalles de implementación para sus miembros. La palabra
- * clave 'abstract' se usa para definir estas clases, así como también los métodos
- * dentro de la misma */
+ * Técnicas Avanzadas */
 
-/* Definición de la clase abstracta */
-abstract class Departamento {
+/* Contructor functions
+ * Cuando se declara una clase en TS, en realidad está creando múltiples
+ * declaraciones al mismo tiempo. El primero es el tipo de la instancia
+ * de la clase */
+
+class Saludar {
   /* Atributos o propiedades */
-  nombre: string;
+  public saludo: string;
   /* Constructor */
-  public constructor( nombreDelDepartamento : string ) {
-    this .nombre = nombreDelDepartamento;
+  public constructor( mensajeSaludo : string ) {
+    this .saludo = mensajeSaludo;
   }
   /* Métodos */
-  public imprimirNombre() : void {
-    console .log( 'Departamento de: ', this .nombre );
-  }
-
-  abstract imprimirReunion() : void;    // Debe ser implementados en las clases derivadas
-}
-
-/* Definición de una clase que hereda de una clase abstracta */
-class DepartamentoContable extends Departamento {
-  /* Constructor */
-  public constructor() {
-    super( 'Contabilidad y auditoría' );    // Constructores en clases derivadas deben llamar a super
-  }
-  /* Métodos */
-  public imprimirReunion() : void {
-    console .log( 'Este departamento se reúne todos los lunes a las 10am' );
-  }
-  public generarReportes() : void {
-    console .log( 'Generación de informes contables' );
+  public darSaludo () {
+    return 'Hola, ' + this .saludo;
   }
 }
 
-/* Crea referencia a una clase de tipo abstracto */
-let departamento: Departamento;
-/* ERRORES que no se deben cometer */
-// departamento1 = new Departamento();  // No se puede crear una instancia a partir de una clase abstracta
-// departamento .generarReportes();     // Este método no esta declarado como abstracto por lo que no se considera existente
+let saludar: Saludar;               // Creamos una variable con el tipo de la Instancia que vamos a crear
+saludar = new Saludar( 'Mundo!' );  // Creamos la Instancia
 
-//departamento .imprimirNombre();
-// departamento .imprimirReunion();     // Este método no esta declarado como abstracto por lo que no se considera existente
-
-/* Crea Instancia apartir de una clase que hereda de una clase abstracta */
-let departamento2 = new DepartamentoContable();
-departamento2 .imprimirNombre();
-departamento2 .imprimirReunion();
+console .log( saludar .darSaludo() );
