ad85f0bdcd800dc091513039d680ac58e1c7a0e2
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-29 09:53:16 -0500

    Types: Tipos complejos - Clases de tipos diferentes (POO en TypeScript)

--- types/js/scripts.js
+++ types/js/scripts.js
@@ -1,15 +1,50 @@
 /* Types: Classes
  * Podemos declarar estructuras de clases en TypeScript */
-var Saludar = /** @class */ (function () {
-    /* Consutructor */
-    function Saludar(mensajeSaludo) {
-        this.saludo = mensajeSaludo;
-    }
-    /* Metodos */
-    Saludar.prototype.getSaludo = function () {
-        return 'Hola, ' + this.saludo;
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
-    return Saludar;
+})();
+/* Definición de la clase Animal */
+var Animal = /** @class */ (function () {
+    /* Constructor */
+    function Animal(nombreDelAnimal) {
+        this.nombre = nombreDelAnimal;
+    }
+    return Animal;
 }());
-var unSaludo = new Saludar('Mundo!');
-console.log('unSaludo ', unSaludo.getSaludo());
+/* Definición de la clase Rinceronte heredando de la clase 'Padre' Animal */
+var Rinoceronte = /** @class */ (function (_super) {
+    __extends(Rinoceronte, _super);
+    /* Constructor */
+    function Rinoceronte() {
+        return _super.call(this, 'Rinoceronte') || this;
+    }
+    return Rinoceronte;
+}(Animal));
+/* Definición de la clase Empleado */
+var Empleado = /** @class */ (function () {
+    /* Constructor */
+    function Empleado(nombreDelEmpleado) {
+        this.nombre = nombreDelEmpleado;
+    }
+    return Empleado;
+}());
+/* Instancias de cada una de las clases */
+var animal = new Animal('Ardilla'), rinoceronte = new Rinoceronte(), empleado = new Empleado('Bernard');
+/* Resultados */
+console.group('Antes de asignar (animal = rinoceronte;)');
+console.log('animal ', animal.nombre);
+console.log('rinoceronte ', rinoceronte.nombre);
+console.groupEnd();
+animal = rinoceronte;
+//animal = empleado /* ERROR: La el objeto 'Animal' no es compatible con el objeto 'Empleado' */
+console.group('Después de asignar (animal = rinoceronte;)');
+console.log('animal ', animal.nombre);
+console.log('rinoceronte ', rinoceronte.nombre);
+console.groupEnd();
--- types/js/scripts.ts
+++ types/js/scripts.ts
@@ -1,22 +1,49 @@
 /* Types: Classes
  * Podemos declarar estructuras de clases en TypeScript */
 
-class Saludar {
-  /* Atributos o Propiedades */
-  saludo: string;
-
-  /* Consutructor */
-  constructor( mensajeSaludo: string ) {
-    this .saludo = mensajeSaludo;
+/* Definición de la clase Animal */
+class Animal {
+  /* Atributos o propiedades */
+  public nombre: string;
+  /* Constructor */
+  constructor( nombreDelAnimal: string ) {
+    this .nombre = nombreDelAnimal;
   }
-
-  /* Metodos */
-  getSaludo() {
-    return 'Hola, ' + this .saludo;
-  }
-
 }
 
-let unSaludo = new Saludar( 'Mundo!' );
+/* Definición de la clase Rinceronte heredando de la clase 'Padre' Animal */
+class Rinoceronte extends Animal {
+  /* Constructor */
+  constructor() {
+    super( 'Rinoceronte' );
+  }
+}
 
-console .log( 'unSaludo ', unSaludo .getSaludo() );
+/* Definición de la clase Empleado */
+class Empleado {
+  /* Atributos o propiedades */
+  private nombre: string;
+  /* Constructor */
+  constructor( nombreDelEmpleado: string ) {
+    this .nombre = nombreDelEmpleado;
+  }
+}
+
+/* Instancias de cada una de las clases */
+let animal      = new Animal( 'Ardilla' ),
+    rinoceronte = new Rinoceronte(),
+    empleado    = new Empleado( 'Bernard' );
+
+/* Resultados */
+console .group( 'Antes de asignar (animal = rinoceronte;)' );
+  console .log( 'animal ', animal .nombre );
+  console .log( 'rinoceronte ', rinoceronte .nombre );
+console .groupEnd();
+
+animal = rinoceronte;
+//animal = empleado /* ERROR: La el objeto 'Animal' no es compatible con el objeto 'Empleado' */
+
+console .group( 'Después de asignar (animal = rinoceronte;)' );
+  console .log( 'animal ', animal .nombre );
+  console .log( 'rinoceronte ', rinoceronte .nombre );
+console .groupEnd();
