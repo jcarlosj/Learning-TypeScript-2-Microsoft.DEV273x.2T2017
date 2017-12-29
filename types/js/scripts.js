c64f97af4bd37d26b94aedc7c514e3cee8e1bee1
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-29 10:33:52 -0500

    Types: Tipos complejos - Clases SIN 'Accessors' (Getters & Setters)

--- types/js/scripts.js
+++ types/js/scripts.js
@@ -1,20 +1,13 @@
 /* Types: Classes
  * Podemos declarar estructuras de clases en TypeScript */
-/* Definición de la clase Pulpo */
-var Pulpo = /** @class */ (function () {
-    function Pulpo(elNombre) {
-        this.numeroDeTentaculos = 8; // Obliga a definir un valor en la declaración
-        this.nombre = elNombre; // o Obliga a definir un valor en el constructor (dinámino o no)
+/* Definición de la clase sin 'Accessors' (Getters & Setters) */
+var Empleado = /** @class */ (function () {
+    function Empleado() {
     }
-    return Pulpo;
+    return Empleado;
 }());
-/* Instancia */
-var pulpo = new Pulpo('Octopus');
-/* Si intentamos realizar cambios a los Atributos nos dará ERROR (pues son de solo lectura) */
-// pulpo .nombre = 'Ahora me llamo Jorge'; // ERROR
-// pulpo .numeroDeTentaculos = 5 // ERROR
-/* Resultados */
-console.group('Modificadores (readonly)');
-console.log('Nombre: ', pulpo.nombre);
-console.log('# tentaculos: ', pulpo.numeroDeTentaculos);
-console.groupEnd();
+var empleado = new Empleado();
+empleado.nombre_completo = 'Jorge Robles';
+if (empleado.nombre_completo) {
+    console.log('Nombre: ', empleado.nombre_completo);
+}
--- types/js/scripts.ts
+++ types/js/scripts.ts
@@ -1,25 +1,15 @@
 /* Types: Classes
  * Podemos declarar estructuras de clases en TypeScript */
 
-/* Definición de la clase Pulpo */
-class Pulpo {
-  /* Atributos tipo readonly */
-  readonly nombre: string;
-  readonly numeroDeTentaculos = 8;    // Obliga a definir un valor en la declaración
-  public constructor( elNombre: string ) {
-    this .nombre = elNombre;          // o Obliga a definir un valor en el constructor (dinámino o no)
-  }
+/* Definición de la clase sin 'Accessors' (Getters & Setters) */
+class Empleado {
+  /* Atributos o propiedades */
+  nombre_completo : string;
 }
 
-/* Instancia */
-let pulpo = new Pulpo( 'Octopus' );
+let empleado = new Empleado();
+empleado .nombre_completo = 'Jorge Robles';
 
-/* Si intentamos realizar cambios a los Atributos nos dará ERROR (pues son de solo lectura) */
-// pulpo .nombre = 'Ahora me llamo Jorge'; // ERROR
-// pulpo .numeroDeTentaculos = 5 // ERROR
-
-/* Resultados */
-console .group( 'Modificadores (readonly)' );
-  console .log( 'Nombre: ', pulpo .nombre );
-  console .log( '# tentaculos: ', pulpo .numeroDeTentaculos );
-console .groupEnd();
+if( empleado .nombre_completo ) {
+  console .log( 'Nombre: ', empleado .nombre_completo );
+}
