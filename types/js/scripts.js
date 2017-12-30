7adebd83901e404258183f8b06e31114757b4801
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-30 12:07:27 -0500

    Types: Tipos complejos - Compatibilidad estructural verificación de argumentos de llamada a función

--- types/js/scripts.js
+++ types/js/scripts.js
@@ -1,5 +1,13 @@
 /* Types: Compatibilidad estructural */
 var persona1, persona2 = { nombre: 'Alicia', localizacion: 'Barranquilla' };
+/* La misma regla de asignación se usa cuando se verifican argumentos de llamada de función
+   Este proceso de comparación se lleva acabo de forma recursiva, explorando el tipo de cada
+   miembro y sub-miembro */
+function saludar(persona) {
+    alert('Hola, ' + persona.nombre);
+}
+saludar(persona2);
+/* Resultado */
 console.group('Antes de asignar persona1 = persona2;');
 console.log('persona1 ', persona1);
 console.log('persona2 ', persona2);
--- types/js/scripts.ts
+++ types/js/scripts.ts
@@ -8,6 +8,16 @@ interface Mencionado {
 let persona1: Mencionado,
     persona2 = { nombre: 'Alicia', localizacion: 'Barranquilla' };
 
+/* La misma regla de asignación se usa cuando se verifican argumentos de llamada de función
+   Este proceso de comparación se lleva acabo de forma recursiva, explorando el tipo de cada
+   miembro y sub-miembro */
+function saludar( persona : Mencionado ) {
+  alert( 'Hola, ' + persona .nombre );
+}
+
+saludar( persona2 );
+
+/* Resultado */
 console .group( 'Antes de asignar persona1 = persona2;' );
   console .log( 'persona1 ', persona1 );
   console .log( 'persona2 ', persona2 );
