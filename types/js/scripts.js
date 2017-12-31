6503d5017fbed4766ddda194d3bf7fce201299af
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-31 10:22:19 -0500

    Types: Tipos complejos - Escribiendo el tipo de función

--- types/js/scripts.js
+++ types/js/scripts.js
@@ -9,3 +9,15 @@ var add_2 = function (num1, num2) {
 };
 console.log('Funcion nombrada : ', add_1(2, 3));
 console.log('Funcion anónima : ', add_2(2, 3));
+/* Las mismas funcion escrita con la sintaxis de funciones flecha */
+var add_3 = function (num1, num2) {
+    return num1 + num2;
+};
+console.log('Otra Funcion ', add_3(2, 3));
+/* Mientras los tipos de parámetro se alinien, se considera un tipo válido
+   para la función, independiente de los nombres que le dé a los parámetros
+   en el tipo de función */
+var add_4 = function (numero1, numero2) {
+    return numero1 + numero2;
+};
+console.log('Otra Funcion (nombres diferentes)', add_4(2, 3));
--- types/js/scripts.ts
+++ types/js/scripts.ts
@@ -12,3 +12,22 @@ let add_2 = function( num1: number, num2: number ) : number {
 
 console .log( 'Funcion nombrada : ', add_1( 2, 3 ) );
 console .log( 'Funcion anónima : ', add_2( 2, 3 ) );
+
+/* Las mismas funcion escrita con la sintaxis de funciones flecha */
+let add_3: ( num1: number, num2: number ) =>
+  number = function ( num1 : number, num2 : number) : number {
+    return num1 + num2;
+  }
+
+console .log( 'Otra Funcion ', add_3( 2, 3 ) );
+
+/* Mientras los tipos de parámetro se alinien, se considera un tipo válido
+   para la función, independiente de los nombres que le dé a los parámetros
+   en el tipo de función */
+
+let add_4: ( num1: number, num2: number ) =>
+ number = function ( numero1 : number, numero2 : number) : number {
+   return numero1 + numero2;
+ }
+
+console .log( 'Otra Funcion (nombres diferentes)', add_4( 2, 3 ) );
