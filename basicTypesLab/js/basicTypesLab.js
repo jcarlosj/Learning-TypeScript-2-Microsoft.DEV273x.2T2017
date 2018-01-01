5f65e9fa11c646100572c72575f87b672af41c1a
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-01-01 15:41:22 -0500

    Types: Lab02 - Creamos la clase 'Fondo'

--- basicTypesLab/js/basicTypesLab.js
+++ basicTypesLab/js/basicTypesLab.js
@@ -1,21 +1,12 @@
 /* basicTypesLab File */
-window.onload = function () {
-    var color = "green", squareSizeNum = 100, squareSize = squareSizeNum + "px", button = document.createElement('button'), // Agrega el elemento HTML 'button' a la variable 'button'
-    div = document.createElement('div'); // Agrega el elemento HTML 'div' a la variable 'div'
-    button.textContent = "Change Color"; // Agrega el texto al elemento HTML 'button'
-    /* Asignamos la función a una variable */
-    var cambioColor = function (elemento, color) {
-        elemento.style.backgroundColor = color; // Agrega una 'Assertion' o (elemento as HTMLElement)
+var Fondo = /** @class */ (function () {
+    /* Constructor */
+    function Fondo(div) {
+        this.div = div;
+    }
+    Fondo.prototype.cambiarColor = function (color) {
+        this.div.style.backgroundColor = color;
         return true;
     };
-    /* Agregamos un ancho y altura al elemento div */
-    div.style.width = squareSize;
-    div.style.height = squareSize; // La Assertion funciona también si la declaramos así
-    /* Agregamos un evento al botón */
-    button.onclick = function (evento) {
-        cambioColor(div, color);
-    };
-    // Agrega los elementos HTML creados al cuerpo del documento
-    document.body.appendChild(button);
-    document.body.appendChild(div);
-};
+    return Fondo;
+}());
--- basicTypesLab/js/basicTypesLab.ts
+++ basicTypesLab/js/basicTypesLab.ts
@@ -1,31 +1,15 @@
 /* basicTypesLab File */
 
-window.onload = function() {
-  let color         : string  = "green",
-      squareSizeNum : number  = 100,
-      squareSize    : string  = `${ squareSizeNum }px`,
-      button        : Element = document .createElement( 'button' ),    // Agrega el elemento HTML 'button' a la variable 'button'
-      div           : Element = document .createElement( 'div' );       // Agrega el elemento HTML 'div' a la variable 'div'
-
-  button .textContent = "Change Color";                                 // Agrega el texto al elemento HTML 'button'
-
-  /* Asignamos la función a una variable */
-  let cambioColor: Function = ( elemento: Element, color: string ) : boolean => {
-    ( <HTMLElement> elemento ) .style .backgroundColor = color;       // Agrega una 'Assertion' o (elemento as HTMLElement)
-
+class Fondo {
+  /* Atributos */
+  div : Element;
+  /* Constructor */
+  constructor( div : Element ) {
+    this .div = div;
+  }
+  cambiarColor( color : string ) : boolean {
+    ( this .div as HTMLElement ) .style .backgroundColor = color;
     return true;
   }
-
-  /* Agregamos un ancho y altura al elemento div */
-  ( <HTMLElement> div ) .style .width = squareSize;
-  ( div as HTMLElement ) .style .height = squareSize;               // La Assertion funciona también si la declaramos así
-  /* Agregamos un evento al botón */
-  ( button as HTMLElement ) .onclick = ( evento ) => {
-    cambioColor( div, color );
-  }
-
-  // Agrega los elementos HTML creados al cuerpo del documento
-  document .body .appendChild(button);
-  document .body .appendChild(div);
 
 }
