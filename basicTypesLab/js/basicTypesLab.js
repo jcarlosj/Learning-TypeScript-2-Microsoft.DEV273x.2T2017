e47aace3b3667f8ce5d002717564746d348556db
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-01-01 16:25:07 -0500

    Types: Lab02 - Creamos un 'Array' con 4 objetos con 2 elementos HTML cada uno (button, div)

--- basicTypesLab/js/basicTypesLab.js
+++ basicTypesLab/js/basicTypesLab.js
@@ -1,4 +1,5 @@
 /* basicTypesLab File */
+//window.onload = function () {    /* A cambio de llamar el JS en el final del body */
 /* Crea clase */
 var Fondo = /** @class */ (function () {
     /* Constructor */
@@ -14,8 +15,18 @@ var Fondo = /** @class */ (function () {
 /* Crea Conjunto de Elementos Enum */
 var Colores;
 (function (Colores) {
-    Colores[Colores["Verde"] = 0] = "Verde";
-    Colores[Colores["Rojo"] = 1] = "Rojo";
-    Colores[Colores["Azul"] = 2] = "Azul";
-    Colores[Colores["Naranja"] = 3] = "Naranja";
+    Colores[Colores["Green"] = 0] = "Green";
+    Colores[Colores["Red"] = 1] = "Red";
+    Colores[Colores["Blue"] = 2] = "Blue";
+    Colores[Colores["Orange"] = 3] = "Orange";
 })(Colores || (Colores = {}));
+/* Creamos un Array (Para unir todo lo anterior) */
+var elementosHTML = [], squareSizeNum = 100, squareSize = squareSizeNum + "px";
+/* Llenamos el Array con 4 Objetos con dos Elementos HTML cada uno de ellos */
+for (var i = 0; i < 4; i++) {
+    elementosHTML.push({
+        'div': document.createElement('div'),
+        'button': document.createElement('button') // Crea elemento HTML button
+    });
+}
+//}
--- basicTypesLab/js/basicTypesLab.ts
+++ basicTypesLab/js/basicTypesLab.ts
@@ -1,30 +1,46 @@
 /* basicTypesLab File */
+//window.onload = function () {    /* A cambio de llamar el JS en el final del body */
+  /* Crea clase */
+  class Fondo {
+    /* Atributos */
+    div : Element;
+    /* Constructor */
+    constructor( div : Element ) {
+      this .div = div;
+    }
+    cambiarColor( color : string ) : boolean {
+      ( this .div as HTMLElement ) .style .backgroundColor = color;
+      return true;
+    }
 
-/* Crea clase */
-class Fondo {
-  /* Atributos */
-  div : Element;
-  /* Constructor */
-  constructor( div : Element ) {
-    this .div = div;
   }
-  cambiarColor( color : string ) : boolean {
-    ( this .div as HTMLElement ) .style .backgroundColor = color;
-    return true;
+
+  /* Crea Interface */
+  interface ConjuntoElementos {
+    'div'    : Element,
+    'button' : Element
   }
 
-}
+  /* Crea Conjunto de Elementos Enum */
+  enum Colores {
+    Green,
+    Red,
+    Blue,
+    Orange
+  }
 
-/* Crea Interface */
-interface ConjuntoElementos {
-  'div'    : Element,
-  'button' : Element
-}
+  /* Creamos un Array (Para unir todo lo anterior) */
+  let elementosHTML : Array<ConjuntoElementos> = [],
+      squareSizeNum : number = 100,
+      squareSize : string = `${ squareSizeNum }px`;
 
-/* Crea Conjunto de Elementos Enum */
-enum Colores {
-  Verde,
-  Rojo,
-  Azul,
-  Naranja
-}
+  /* Llenamos el Array con 4 Objetos con dos Elementos HTML cada uno de ellos */
+  for( let i: number = 0; i < 4; i++ ) {
+    elementosHTML .push({
+      'div'    : document .createElement( 'div' ),      // Crea elemento HTML div
+      'button' : document .createElement( 'button' )    // Crea elemento HTML button
+    });
+  }
+
+
+//}
