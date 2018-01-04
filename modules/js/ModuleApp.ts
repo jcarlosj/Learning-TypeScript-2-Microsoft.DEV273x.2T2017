a63da038dca8cfd51801304e40d699f90fd4efc0
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-01-04 10:53:03 -0500

    Modules: Renombrar importación desde un módulo

--- modules/js/ModuleApp.ts
+++ modules/js/ModuleApp.ts
@@ -1,4 +1,4 @@
 /* Importar una sola exportación desde un módulo */
-import { ZipCodeValidator } from './ZipCodeValidator';
+import { ZipCodeValidator as ZCV } from './ZipCodeValidator';
 
-let myValidator = new ZipCodeValidator();
+let myValidator = new ZCV();
