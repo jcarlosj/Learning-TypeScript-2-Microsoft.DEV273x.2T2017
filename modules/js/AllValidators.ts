57168b2e7a60559b16ed181395756a365ddedb78
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-01-04 10:47:36 -0500

    Modules: Combinación de exportaciones para un mismo módulo

---
+++ modules/js/AllValidators.ts
@@ -0,0 +1,6 @@
+/* Combinando exportaciones
+ * Un Módulo puede envolver uno o más modulos y combinar todas sus exportaciones */
+
+ export * from './StringValidator';       // Exporta la Interface
+ export * from './ZipCodeValidator';      // Exporta la Clase
+ // ... y así generando un listado de exportaciones del mismo modulo 
