781bb39f231c3521162defa45f00946aefb3234d
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-01-04 10:33:41 -0500

    Modules: Exportar y renombrar si va a ser consumido en diferentes partes de la app

--- modules/js/ZipCodeValidator.ts
+++ modules/js/ZipCodeValidator.ts
@@ -2,8 +2,13 @@
 export const numberRegexp = /^[0-9]+$/;
 
 /* Crea y exporta una Clase que implementa una 'Interface' */
-export class ZipCodeValidator implements StringValidator {
+class ZipCodeValidator implements StringValidator {
   isAcceptable( s : string ) {
     return s .length === 5 && numberRegexp .test( s );
   }
 }
+
+/* Cuando las exportaciones deben renombrarse de acuerdo a los ficheros que los consumen
+ * podemos renombrarlos de la siguiente manera */
+export { ZipCodeValidator };                  // Exportación corriente
+export { ZipCodeValidator as mainValidator }  // Exportando con otro nombre 'mainValidator'
