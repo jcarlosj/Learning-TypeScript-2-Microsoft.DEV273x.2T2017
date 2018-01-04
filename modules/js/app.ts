48f9c4e4d9732b255e74ea9ef9ef88f280840158
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-01-04 13:08:24 -0500

    Modules: Módulos Isomórficos

--- modules/js/app.ts
+++ modules/js/app.ts
@@ -1,6 +1,10 @@
 /* Hacemos la referencía al archivo donde lo requerimos */
-/// <reference path="declaraciones.d.ts" />
+/// <reference path="math-lib.d.ts" />
 
-/* Todas las importaciones de un módulo abreviado tendrán el tipo 'any' */
-import x, { y } from 'new-module';
-x( y );
+/* La biblioteca se puede usar como una importación dentro de los módulos */
+import { isPrime } from 'math-lib';
+isPrime( 2 );
+//mathLib .isPrime( 2 );      // ERROR No puede usar la definición global desde dentro de un módulo
+
+/* NOTA: También se puede usar como una variable global, pero solo debtro de una secuencia de comandos.
+ *       (Un script es un archivo sin importaciones ni exportaciones) */
--- modules/js/declaraciones.d.ts
+++
@@ -1,4 +0,0 @@
-/* Shorthand - Modulos Ambientales
- * Si no desea tomarse el tiempo para escribir declaraciones antes de poder
- * usar un nuevo módulo, puede usar una declaración abreviada para comenzar rápidamente  */
- declare module 'new-module';
