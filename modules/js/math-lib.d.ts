a81c846cc5f0f92ed586815eb6e3301781ec125e
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-01-05 11:07:51 -0500

    Modules: Módulos Relativos VS No Relativos

--- modules/js/math-lib.d.ts
+++
@@ -1,6 +0,0 @@
-/* Módulos isomórficos o UMD
- * Algunas bibliotecas están diseñadas para ser utilizadas en muchos cargadores
- * de módulos o sin carga de módulos (variables globales). Se pueden acceder a estas
- * bibliotecas a través de una importación de una variable global*/
-export const isPrime( x : number ) : boolean;
-export as namespace mathLib;
